d55aafa72459043e461af07241f4abcd3e7c90b0
NgirfanulMangali <metalqwerty0@gmail.com>
2025-05-27 23:14:08 -0400

    done

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,8 @@
-console.log('Testing our first TypeScript file');
+export function countBy(x: number, n: number): number[] {
+
+    for (let i = x; i < 10; i += n) {
+        return [i]
+    }
+
+}
+console.log(countBy(1, 10))
