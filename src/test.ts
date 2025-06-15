9c1ddbc0b1a6486aedbae63621af11c74ca55690
NgirfanulMangali <metalqwerty0@gmail.com>
2025-06-15 15:59:41 -0400

    done

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,6 @@
-export function binaryArrayToNumber(arr: number[]): number {
-
-    const invite = arr.join("")
-    const result = parseInt(invite, 2)
-    return result
+export function uniqueInOrder(iterable: string | (string | number)[]): (string | number)[] {
+    const s = new Set(iterable);
+    const arr = [...s];
+    return arr
 };
-console.log(binaryArrayToNumber([1, 1, 1, 1]))
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))
