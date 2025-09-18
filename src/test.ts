c0ff376138608ac53eb11c2a3a5971c96c3daefa
NgirfanulMangali <metalqwerty0@gmail.com>
2025-09-17 18:48:14 -0700

    Training JS #9: loop statement --while and do..while

--- src/test.ts
+++ src/test.ts
@@ -1,32 +1,16 @@
-function howManydays(n){
-  // getDay() is a method of Date() - we will learn this later
-  
-  switch (n){
-    case 1:
-      return 31
-    case 3:
-      return 31
-    case 5:
-      return 31
-    case 7:
-      return 31
-    case 8:
-     return 31
-    case 10:
-      return 31
-    case 12:
-      return 31
-    case 4:
-      return 30
-    case 6:
-      return 30
-      case 9:
-      return 30
-      case 11:
-      return 30
-      case 2:
-      return 28
-  }
-  
+function padIt(str, n) {
+  var num = 0;
+  do {
+    if (num % 2 === 0) {
+      // kalau genap → taruh di kiri
+      str = "*" + str;
+    } else {
+      // kalau ganjil → taruh di kanan
+      str = str + "*";
+    }
+    num++;
+  } while (num < n)
+  return str;
 }
-console.log(howManydays(2))
+
+console.log(padIt("a", 5)); // ***a**
